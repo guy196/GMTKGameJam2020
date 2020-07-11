284d9aeb840177bc3d872f272b60a81732590453
guy196 <guy.shalev01@gmail.com>
2020-07-11 03:07:02 +0300

    a
    a

---
+++ Overboard/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text Text;
+    public float timeLeft;
+
+    [System.Obsolete]
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        Text.text = timeLeft.ToString("F2");
+        if (timeLeft < 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
